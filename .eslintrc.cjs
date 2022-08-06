849bbfdbb148d8bda4d274fb6cd421c283123e33
Juan Blasco <juablave@gmail.com>
2022-08-06 12:07:26 +0200

    Added husky testing

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,5 +21,6 @@ module.exports = {
     // suppress errors for missing 'import React' in files
    "react/react-in-jsx-scope": "off",
    "import/prefer-default-export": "off",
+   "react/prop-types": "off"
   },
 };
